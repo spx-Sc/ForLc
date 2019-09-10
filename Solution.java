0cf84ddb98ef11ef6915e6ee01790bfc7aaa413c
handuan <549438391@qq.com>
2019-09-09 23:32:10 -0500

    Update!

--- Solution.java
+++ Solution.java
@@ -796,6 +796,8 @@ public class Solution {
     }
 */
 
+
+    /*
     public int minPathSum(int[][] grid) {
         for (int i = 1; i < grid.length; i++) {
             grid[i][0] = grid[i-1][0] + grid[i][0];
@@ -810,7 +812,33 @@ public class Solution {
         }
         return grid[grid.length-1][grid[0].length-1];
     }
+*/
 
+    public void setZeroes(int[][] matrix) {
+        //record the positions of zeros
+        List<Integer> row = new ArrayList<>();
+        List<Integer> col = new ArrayList<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    row.add(i);
+                    col.add(j);
+                }
+            }
+        }
+
+        for (int i : row) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+        for (int i : col) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[j][i] = 0;
+            }
+        }
+
+    }
 
 
 
